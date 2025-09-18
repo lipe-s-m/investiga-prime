35469f2872c2cdfca4e8d39ed6924fd715f62453
lipe-s-m <felipe.s.m1609@gmail.com>
2025-09-18 14:34:43 -0300

    :sparkles: add input search

--- src/app/Pages/FAQ/faq.ts
+++ src/app/Pages/FAQ/faq.ts
@@ -2,14 +2,60 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Card } from './Components/card/card';
 import { CommonModule } from '@angular/common';
 import { questions } from '../../data/questions.mock';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.html',
   styleUrls: ['./faq.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [Card, CommonModule],
+  imports: [Card, CommonModule, FormsModule],
 })
 export default class FaqComponent {
-  questions = questions;
+  listQuestions = questions;
+  inputText: string = '';
+  private debouncing: Function;
+  constructor() {
+    this.debouncing = this.debounce(this.filter.bind(this), 100);
+  }
+  onInputChange(value: string) {
+    this.inputText = value;
+
+    if (this.inputText.length === 0) {
+      this.listQuestions = questions;
+      return;
+    }
+
+    this.debouncing();
+  }
+  debounce(func: any, delay: number) {
+    let timeout: number;
+    return (...args: any) => {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        func.apply(this, args);
+      }, delay);
+    };
+  }
+
+  filter() {
+    let inputIndex = this.inputText.length;
+    this.listQuestions = questions.filter((q) => {
+      if (
+        this.removerAcentos(q.title.toLowerCase()).includes(
+          this.removerAcentos(this.inputText.toLowerCase())
+        )
+      ) {
+        return q;
+      }
+      return;
+    });
+  }
+  removerAcentos(str: string) {
+    return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+  }
+  onSubmitForm() {
+    event?.preventDefault();
+    this.filter();
+  }
 }
